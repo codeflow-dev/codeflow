93f5403378f8b250dc01cd77b9013d5a13a76689
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2023-07-10 22:57:53 +0600

    Problem Statement page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html", "./dashboard.html", "contests.html", "contests.html", "host.html", "./assets/**/*.svg"],
+    content: ["./*.html", "./assets/**/*.svg"],
     theme: {
         extend: {
             colors: {
                 blue: {
                     200: "#1F2743",
-                    300: "#1E293B",
+                    300: "#1D2543",
                     400: "#131B2E",
                     500: "#151B31",
+                    600: "#12172C",
                     800: "#101425",
                 },
                 indigo: {
