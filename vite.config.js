e29a860aef8ff782eb6e7fa9386a2175d3aa155d
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2023-07-17 03:27:13 +0600

    Active link background color

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import vituum from "vituum";
 import posthtml from "@vituum/vite-plugin-posthtml";
+import modules from "posthtml-modules";
 
 export default {
     plugins: [
         vituum(),
         posthtml({
             root: "./src",
+            plugins: [modules({ attributeAsLocals: true })],
         }),
     ],
     server: {
