87be253d25026e24e9aa5e25cf0863814465ec62
alif03122000 <u1904050@student.cuet.ac.bd>
2023-07-17 20:42:07 +0600

    Problems Rout

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { connect } from "mongoose";
-import express, { json } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.js";
 import contestsRouter from "./routes/contests.js";
+import problemsRouter from "./routes/problems.js";
 
 dotenv.config();
 
@@ -13,10 +14,10 @@ connect(process.env.MONGODB_URI || "mongodb://localhost/codeflow");
 
 const app = express();
 
-app.use(json());
+app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", [authRouter, contestsRouter]);
+app.use("/api", [authRouter, contestsRouter, problemsRouter]);
 
 app.listen(port, () => {
     console.log("Server started");
