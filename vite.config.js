bfc5b11bac3a059cd80aabf84134b7749a9d4fcf
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2023-07-22 15:05:20 +0600

    Plugin for router

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import vituum from "vituum";
 import posthtml from "@vituum/vite-plugin-posthtml";
 import modules from "posthtml-modules";
+import { defineConfig } from "vite";
 
-export default {
+const routePlugin = () => ({
+    name: "route-html",
+    configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+            if (!req.originalUrl.endsWith(".html")) {
+                req.originalUrl += ".html";
+            }
+            next();
+        });
+    },
+});
+
+export default defineConfig({
     plugins: [
         vituum(),
         posthtml({
             root: "./src",
             plugins: [modules({ attributeAsLocals: true })],
         }),
+        routePlugin(),
     ],
     server: {
         port: 5173,
@@ -19,4 +33,4 @@ export default {
             },
         },
     },
-};
+});
