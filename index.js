921991355d17c29f399dd722e0dd1877ec8cbacb
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2023-07-22 17:34:46 +0600

    Contest page

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.js";
 import contestsRouter from "./routes/contests.js";
 import problemsRouter from "./routes/problems.js";
+import { renderFile } from "ejs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url); // Get the current file's path
+const __dirname = dirname(__filename); // Get the current directory name
+
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -17,6 +24,17 @@ app.use(json());
 app.use(cookieParser());
 
 app.use("/api", [authRouter, contestsRouter, problemsRouter]);
+
+app.use(express.static("dist/"));
+app.use("/assets", express.static("assets/"));
+
+app.set("view engine", "html");
+app.set("views", path.join(__dirname, "dist"));
+app.engine("html", renderFile);
+
+app.get("/contest/:id", (req, res) => {
+    res.render("contest");
+});
 
 app.listen(port, () => {
     console.log("Server started");
--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,6 @@ import posthtml from "@vituum/vite-plugin-posthtml";
 import modules from "posthtml-modules";
 import { defineConfig } from "vite";
 
-const routePlugin = () => ({
-    name: "route-html",
-    configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-            if (!req.originalUrl.endsWith(".html")) {
-                req.originalUrl += ".html";
-            }
-            next();
-        });
-    },
-});
-
 export default defineConfig({
     plugins: [
         vituum(),
@@ -22,7 +10,6 @@ export default defineConfig({
             root: "./src",
             plugins: [modules({ attributeAsLocals: true })],
         }),
-        routePlugin(),
     ],
     server: {
         port: 5173,
