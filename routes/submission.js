59e670b669c34d25e7118c070d11ca55c6f0b858
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2023-07-25 02:52:04 +0600

    Implement judging in API

--- routes/submission.js
+++ routes/submission.js
@@ -5,15 +5,28 @@ import { randomBytes } from "crypto";
 import { tmpdir } from "os";
 import path from "path";
 import { writeFile } from "fs/promises";
+import Problem from "../models/problem.js";
 
 const router = Router();
 
-async function compileCpp(tmpFileName) {
+async function compileCpp(tmpFileName, problem) {
     try {
         await execa("g++", [tmpFileName, "-o", tmpFileName + ".bin"]);
     } catch (err) {
         const { stderr } = err;
         return { message: "Compilation Error", stderr };
+    }
+    for (let i = 0; i < problem.testCases.length; i++) {
+        const testCase = problem.testCases[i];
+        try {
+            const { stdout } = await execa(tmpFileName + ".bin", [], { input: testCase.input });
+            if (stdout != testCase.output) {
+                return { message: `Wrong answer on test case ${i + 1}`, stdout };
+            }
+        } catch (err) {
+            const { stderr } = err;
+            return { message: `Runtime Error on test case ${i + 1}`, stderr };
+        }
     }
     try {
         const { stdout } = await execa(tmpFileName + ".bin", [], { input: "5 5" });
@@ -26,11 +39,15 @@ async function compileCpp(tmpFileName) {
 
 router.post("/submission", verifyJWT, async (req, res) => {
     try {
-        const { code, language } = req.body;
+        const { code, language, id } = req.body;
+        const p = await Problem.findById(id);
+        if (!p) {
+            throw new Error("Problem not found");
+        }
         const tmpFileName = path.join(tmpdir(), randomBytes(32).toString("hex") + "." + language);
         await writeFile(tmpFileName, code);
         if (language == "cpp") {
-            res.status(200).json(await compileCpp(tmpFileName));
+            res.status(200).json(await compileCpp(tmpFileName, p));
         } else {
             res.status(200).json(r);
         }
