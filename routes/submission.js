128ba64796efba6fe14e1b170eeaf974711fe33d
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2023-07-25 15:24:26 +0600

    Playground page backend works

--- routes/submission.js
+++ routes/submission.js
@@ -3,13 +3,57 @@ import { verifyJWT } from "../midlewares/verify_jwt.js";
 import { execa } from "execa";
 import { randomBytes } from "crypto";
 import { tmpdir } from "os";
-import path from "path";
-import { writeFile } from "fs/promises";
+import path, { dirname } from "path";
+import { readFile, writeFile } from "fs/promises";
 import Problem from "../models/problem.js";
 
 const router = Router();
 
-async function compileCpp(tmpFileName, problem) {
+router.post("/submission", verifyJWT, async (req, res) => {
+    try {
+        const { code, language, id } = req.body;
+        const p = await Problem.findById(id);
+        if (!p) {
+            throw new Error("Problem not found");
+        }
+        const tmpFileName = path.join(tmpdir(), randomBytes(32).toString("hex") + "." + language);
+        await writeFile(tmpFileName, code);
+        if (language == "cpp") {
+            res.status(200).json(await judgeCpp(tmpFileName, p));
+        } else {
+            res.status(200).json({
+                message: "Language not supported",
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(400).json(err);
+    }
+});
+
+router.post("/playground", async (req, res) => {
+    try {
+        const { code, language, input } = req.body;
+        const tmpFileName = path.join(tmpdir(), randomBytes(32).toString("hex") + "." + language);
+        await writeFile(tmpFileName, code);
+        if (language == "cpp") {
+            res.status(200).json(await runCpp(tmpFileName, input));
+        } else if (language == "py") {
+            res.status(200).json(await runPython(tmpFileName, input));
+        } else if (language == "java") {
+            res.status(200).json(await runJava(tmpFileName, input));
+        } else {
+            res.status(200).json({
+                message: "Language not supported",
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(400).json(err);
+    }
+});
+
+async function judgeCpp(tmpFileName, problem) {
     try {
         await execa("g++", [tmpFileName, "-o", tmpFileName + ".bin"]);
     } catch (err) {
@@ -28,33 +72,59 @@ async function compileCpp(tmpFileName, problem) {
             return { message: `Runtime Error on test case ${i + 1}`, stderr };
         }
     }
+}
+
+async function runCpp(tmpFileName, input) {
     try {
-        const { stdout } = await execa(tmpFileName + ".bin", [], { input: "5 5" });
-        return { message: "Success", stdout };
+        await execa("g++", [tmpFileName, "-o", tmpFileName + ".bin"]);
     } catch (err) {
         const { stderr } = err;
-        return { message: "Runtime Error", stderr };
+        return { message: "Compilation Error", stdout: stderr };
+    }
+    try {
+        const { stdout } = await execa(tmpFileName + ".bin", [], { input });
+        return { message: `Success`, stdout };
+    } catch (err) {
+        const { stderr } = err;
+        return { message: `Runtime Error`, stdout: stderr };
     }
 }
 
-router.post("/submission", verifyJWT, async (req, res) => {
+async function runPython(tmpFileName, input) {
     try {
-        const { code, language, id } = req.body;
-        const p = await Problem.findById(id);
-        if (!p) {
-            throw new Error("Problem not found");
-        }
-        const tmpFileName = path.join(tmpdir(), randomBytes(32).toString("hex") + "." + language);
-        await writeFile(tmpFileName, code);
-        if (language == "cpp") {
-            res.status(200).json(await compileCpp(tmpFileName, p));
-        } else {
-            res.status(200).json(r);
-        }
+        const { stdout } = await execa("python3", [tmpFileName], { input });
+        return { message: `Success`, stdout };
     } catch (err) {
-        console.error(err);
-        res.status(400).json(err);
+        const { stderr } = err;
+        return { message: `Runtime Error`, stdout: stderr };
     }
-});
+}
+
+async function runJava(tmpFileName, input) {
+    const code = await readFile(tmpFileName);
+    const regex = /public\s+class\s+([a-zA-Z_$][a-zA-Z\d_$]*)/;
+    const match = code.toString().match(regex);
+    let className;
+    if (match && match[1]) {
+        className = match[1];
+    } else {
+        return { message: "Public class not found", stdout: "" };
+    }
+    tmpFileName = path.join(tmpdir(), className + ".java");
+    await writeFile(tmpFileName, code);
+    try {
+        await execa("javac", [tmpFileName], { cwd: dirname(tmpFileName) });
+    } catch (err) {
+        const { stderr } = err;
+        return { message: "Compilation Error", stdout: stderr };
+    }
+    try {
+        const { stdout } = await execa("java", [className], { cwd: dirname(tmpFileName), input });
+        return { message: `Success`, stdout };
+    } catch (err) {
+        const { stderr } = err;
+        return { message: `Runtime Error`, stdout: stderr };
+    }
+}
 
 export default router;
--- src/scripts/playground.js
+++ src/scripts/playground.js
@@ -5,11 +5,28 @@ import { cpp } from "@codemirror/lang-cpp";
 import { Compartment } from "@codemirror/state";
 import { oneDark } from "@codemirror/theme-one-dark";
 
-
 let language = new Compartment(),
     theme = new Compartment();
 
 let editor = new EditorView({
     extensions: [basicSetup, language.of(cpp()), theme.of(oneDark), keymap.of(indentWithTab)],
     parent: document.getElementById("editor"),
+});
+
+document.getElementById("run").addEventListener("click", async (event) => {
+    event.preventDefault();
+    const code = editor.state.doc.text.join("\n");
+    console.log(code);
+    const language = document.getElementById("language").value;
+    const input = document.getElementById("input").value;
+    const r = await fetch("/api/playground", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code, language, input }),
+    });
+    const j = await r.json();
+    document.getElementById("output").value = j.stdout;
 });
