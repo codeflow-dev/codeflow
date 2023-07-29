b29afd967e83d2ecf3bd601556493d85286f5065
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2023-07-30 02:07:20 +0600

    Fix setter middleware

--- midlewares/verify_setter.js
+++ midlewares/verify_setter.js
@@ -3,8 +3,7 @@ import User from "../models/user.js";
 export async function verifySetter(req, res, next) {
     try {
         const user = await User.findById(req.payload.user);
-        if (user.rating >= 1500) user.setter = true;
-        if (!user.setter) {
+        if ((await user.rating()) < 1500) {
             throw new Error("Not setter");
         }
         next();
