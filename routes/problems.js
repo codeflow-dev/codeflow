fabc581ed6329388639de62b74d5df85a84ab66d
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2023-08-02 03:18:40 +0600

    Don't show unpublished problems

--- routes/problems.js
+++ routes/problems.js
@@ -7,19 +7,23 @@ router.get("/problems", async (req, res) => {
     try {
         let data = await Problem.find().populate("contest");
         let result = [];
+        const currentDate = new Date();
         for (let i = 0; i < data.length; i++) {
-            const solvedBy = await Submission.find({ problem: data[i]._id, message: "Accepted" }).countDocuments();
-            let r = {
-                _id: data[i]._id,
-                id: data[i].id,
-                title: data[i].title,
-                rating: data[i].rating,
-                round: data[i].contest.round,
-                solvedBy,
-            };
-            result.push(r);
+            const { contestDate, duration } = data[i].contest;
+            if (data[i].contest.published && new Date(contestDate).getTime() + duration < currentDate) {
+                const solvedBy = await Submission.find({ problem: data[i]._id, message: "Accepted" }).countDocuments();
+                let r = {
+                    _id: data[i]._id,
+                    id: data[i].id,
+                    title: data[i].title,
+                    rating: data[i].rating,
+                    round: data[i].contest.round,
+                    solvedBy,
+                };
+                result.push(r);
+            }
         }
-        console.log(result);
+        // console.log(result);
         res.status(200).json(result);
     } catch (err) {
         console.error(err);
